a9359eb8b9d4376360ca9901fb743c09a8fceca1
khushpreet kaur <kkhushpreetkau00@mylangara.ca>
2024-06-10 23:38:59 -0700

    firebase linked

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+
+// Import the functions you need from the SDKs you need
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALrqZzCRKFwwlJ1jB1ir1k8oTZz1DOLTA",
+  authDomain: "upzy-9530b.firebaseapp.com",
+  databaseURL: "https://upzy-9530b-default-rtdb.firebaseio.com",
+  projectId: "upzy-9530b",
+  storageBucket: "upzy-9530b.appspot.com",
+  messagingSenderId: "448313198620",
+  appId: "1:448313198620:web:6f150df0165df9e8cff7d9",
+  measurementId: "G-MS8KL2ZZ2H"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js" 
-import{ getDatabase, ref , push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
-const appSettings = {
-    databaseURL: "https://realtime-database-c025c-default-rtdb.firebaseio.com/"  
+async function addUser() {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
 
-// url of your realtime database
-
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-
-const shoppingListInDB = ref(database, "ShoppingList");
-// const booksInDB = ref(database, "books");
+addUser();
